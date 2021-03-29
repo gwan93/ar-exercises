7fe7f56d17dc2720078e25812005b2e9c05fee44
Gio Wan <gio_wan@live.ca>
2021-03-29 16:21:35 -0600

    data validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, :length => {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than: 0}
+  validate :must_sell_apparel, on: :create
+
+  def must_sell_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(mens_apparel, "store must sell either mens or womens apparel or both")
+    end
+  end
+
 
 end
